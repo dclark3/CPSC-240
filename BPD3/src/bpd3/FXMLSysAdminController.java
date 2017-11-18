1850f2a20a9a71a1493e4ce220c8a819885717fb
Christina <Christina@10.213.131.45>
2017-11-18 16:10:43 -0500

    SystemAdmin Gui Changes

--- BPD3/src/bpd3/FXMLSysAdminController.java
+++ BPD3/src/bpd3/FXMLSysAdminController.java
@@ -15,6 +15,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +27,97 @@ import javafx.stage.Stage;
  * @author drado_000
  */
 public class FXMLSysAdminController implements Initializable {
+
+    @FXML
+    private Button logOutButton;
+    @FXML
+    private TextField userOfficeMan;
+    @FXML
+    private Button checkOfficeMan;
+    @FXML
+    private Label OfficeManlabel;
+    @FXML
+    private TextField firstnameOfficeMan;
+    @FXML
+    private TextField lastnameOfficeMan;
+    @FXML
+    private TextField emailOfficeMan;
+    @FXML
+    private Button createOfficeMan;
+    @FXML
+    private PasswordField passOfficeMan;
+    @FXML
+    private PasswordField confirmpassOfficeMan;
+    @FXML
+    private Label createOfficeManlabel;
+    @FXML
+    private TextField userWHman;
+    @FXML
+    private Button checkWHman;
+    @FXML
+    private TextField firstnameWHman;
+    @FXML
+    private TextField lastnameWHman;
+    @FXML
+    private TextField emailWHman;
+    @FXML
+    private Button createWHman;
+    @FXML
+    private Label WHmanuserlabel;
+    @FXML
+    private PasswordField passWHman;
+    @FXML
+    private PasswordField confirmpassWHman;
+    @FXML
+    private Label createWHmanlabel;
+    @FXML
+    private TextField userSA;
+    @FXML
+    private Button checkSA;
+    @FXML
+    private TextField firstnameSA;
+    @FXML
+    private TextField lastnameSA;
+    @FXML
+    private TextField emailSA;
+    @FXML
+    private Button createSA;
+    @FXML
+    private Label SAlabel;
+    @FXML
+    private TextField SVname;
+    @FXML
+    private Button checkSV;
+    @FXML
+    private Label SVlabel;
+    @FXML
+    private PasswordField passSA;
+    @FXML
+    private PasswordField confirmpassSA;
+    @FXML
+    private Label createSAlabel;
+    @FXML
+    private TextField changepassUser;
+    @FXML
+    private Button changepass;
+    @FXML
+    private Label changepasslabel;
+    @FXML
+    private PasswordField currentpass;
+    @FXML
+    private PasswordField newpass;
+    @FXML
+    private PasswordField confirmnewpass;
+    @FXML
+    private TextField deleteuser;
+    @FXML
+    private Button confirmdeleteuser;
+    @FXML
+    private Label deleteuserlabel;
+    @FXML
+    private PasswordField systemadminpass;
+    @FXML
+    private PasswordField confirmsystemadminpass;
     
     @FXML
     private void logOutButtonMethod(ActionEvent event) throws IOException {
@@ -31,6 +126,22 @@ public class FXMLSysAdminController implements Initializable {
         Stage projectStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         projectStage.setScene(homePageScene);
         projectStage.show();
+    }
+    
+    
+    
+    private void createOfficeManMethod(ActionEvent event) throws IOException {
+      String un = userOfficeMan.getText();
+       if (un =  ) {
+           OfficeManlabel.setText("Sorry, user already exists");
+           OfficeManlabel.getText();
+       }
+      
+      else {
+           
+              
+              }
+      
     }
     /**
      * Initializes the controller class.
