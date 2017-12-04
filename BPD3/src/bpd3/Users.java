c97bb68da4cdfc7d56746b690ec9591ab24b01b9
Christina <Christina@10.213.131.45>
2017-12-03 22:00:56 -0500

    System Admin changes - mostly done

---
+++ BPD3/src/bpd3/Users.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bpd3;
+import java.util.ArrayList;
+/**
+ *
+ * @author Christina
+ */
+ 
+
+public class Users {
+    public static ArrayList <SysAdmin> adminlist = new ArrayList<SysAdmin>();
+    public static ArrayList <SalesAssociate> salist = new ArrayList<SalesAssociate>();
+    public static ArrayList <OfficeMan> omlist = new ArrayList<OfficeMan>();
+    public static ArrayList <WarehouseMan> wmlist = new ArrayList<WarehouseMan>();
+}
